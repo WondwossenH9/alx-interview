46ebe66d210905558bc8c21c79db8eedcec31279
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-08-26 08:30:54 +0000

    determine the fewest number of coins needed to meet a given amount total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" The makeChange function"""
+
+
+def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet the total
+        If the total is 0 or less, return 0
+        If the total cannot be met by any number of coins return -1
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
