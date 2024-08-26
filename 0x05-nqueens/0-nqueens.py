15d7471302bd9adbada9fda4b05b1dff55c8cfd2
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-08-26 09:12:01 +0000

    fix code error

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
-""" nqueens """
+""" N queens """
 import sys
 
 
 if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens n")
+    print("Usage: nqueens N")
     exit(1)
 
 if not sys.argv[1].isdigit():
-    print("n must be a number")
+    print("N must be a number")
     exit(1)
 
 if int(sys.argv[1]) < 4:
-    print("n must be at least 4")
+    print("N must be at least 4")
     exit(1)
 
 n = int(sys.argv[1])
 
 
-def queens(n, i=0, x=[], y=[], z=[]):
+def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
     if i < n:
         for j in range(n):
-            if j not in x and i + j not in y and i - j not in z:
-                yield from queens(n, i + 1, x + [j], y + [i + j], z + [i - j])
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
-        yield x
+        yield a
 
 
 def solve(n):
     """ solve """
-    m = []
+    k = []
     i = 0
     for solution in queens(n, 0):
         for s in solution:
-            m.append([i, s])
+            k.append([i, s])
             i += 1
-        print(m)
-        m = []
+        print(k)
+        k = []
         i = 0
 
 
